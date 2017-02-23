1f7429e12a88a0b4feefb20fa5369bd937ab9e2a
Felix Sun <sunfmin@gmail.com>
2017-02-23 15:24:49 +0800

    Add humanid

---
+++ humanid.go
@@ -0,0 +1,27 @@
+package humanid
+
+import (
+	"math/rand"
+	"time"
+)
+
+var chars = []rune{
+	/*"0"*/ '1', '2', '3', '4', '5', '6', '7', '8', '9',
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+	'L', 'M', 'N' /*'O'*/, 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+	'W', 'X', 'Y', 'Z',
+}
+
+func New() (id string) {
+	rand.Seed(time.Now().UnixNano())
+	var vals []rune
+	for i := 0; i < 8; i++ {
+		c := chars[rand.Intn(len(chars))]
+		if i == 4 {
+			vals = append(vals, '-')
+		}
+		vals = append(vals, c)
+	}
+	id = string(vals)
+	return
+}
---
+++ humanid_test.go
@@ -0,0 +1,26 @@
+package humanid_test
+
+import (
+	"testing"
+
+	"fmt"
+
+	"github.com/theplant/humanid"
+)
+
+func TestNew(t *testing.T) {
+	for i := 0; i < 20; i++ {
+		id := humanid.New()
+		fmt.Println(id)
+		if len(id) != 9 {
+			t.Error(id)
+		}
+	}
+}
+
+func BenchmarkNew(b *testing.B) {
+	// run the Fib function b.N times
+	for n := 0; n < b.N; n++ {
+		humanid.New()
+	}
+}
