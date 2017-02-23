f9d7debbe996d0fb672842270fb5cea3a4aedfd6
Felix Sun <sunfmin@gmail.com>
2017-02-23 21:05:57 +0800

    Add NewNumber

--- humanid.go
+++ humanid.go
@@ -12,6 +12,10 @@ var chars = []rune{
 	'W', 'X', 'Y', 'Z',
 }
 
+var numchars = []rune{
+	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+}
+
 func New() (id string) {
 	rand.Seed(time.Now().UnixNano())
 	var vals = make([]rune, 9)
@@ -22,6 +26,18 @@ func New() (id string) {
 		} else {
 			vals[i] = c
 		}
+	}
+	id = string(vals)
+	return
+}
+
+func NewNumber() (id string) {
+	rand.Seed(time.Now().UnixNano())
+	var vals = make([]rune, 8)
+	vals[0] = numchars[rand.Intn(len(numchars)-1)+1] // no zero for first.
+	for i := 1; i < 8; i++ {
+		c := numchars[rand.Intn(len(numchars))]
+		vals[i] = c
 	}
 	id = string(vals)
 	return
--- humanid_test.go
+++ humanid_test.go
@@ -18,6 +18,16 @@ func TestNew(t *testing.T) {
 	}
 }
 
+func TestNewNumber(t *testing.T) {
+	for i := 0; i < 20; i++ {
+		id := humanid.NewNumber()
+		fmt.Println(id)
+		if len(id) != 8 {
+			t.Error(id)
+		}
+	}
+}
+
 func BenchmarkNew(b *testing.B) {
 	// run the Fib function b.N times
 	for n := 0; n < b.N; n++ {
